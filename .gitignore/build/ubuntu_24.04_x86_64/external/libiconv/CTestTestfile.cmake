371e5b981b9f9f497a5ebc8447cc61a201542997
bedudu98 <dungdungtran98@gmail.com>
2025-06-04 16:07:00 +0200

    .gitignore file edited

--- .gitignore/build/ubuntu_24.04_x86_64/external/libiconv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dungtran/Dokumente/work/org.gnu.libidn2-libidn2/external/libiconv
-# Build directory: /home/dungtran/Dokumente/work/org.gnu.libidn2-libidn2/build/ubuntu_24.04_x86_64/external/libiconv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- .gitignore/build/ubuntu_24.04_x86_64/libidn2/CMakeFiles/TARGET_libidn2.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for TARGET_libidn2.
-# This may be replaced when dependencies are built.
