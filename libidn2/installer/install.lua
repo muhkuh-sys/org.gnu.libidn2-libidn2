ce583f66b248e6db4e1d15aae47c59ea1ef5eafd
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2025-06-12 17:12:16 +0200

    Add the pkg-config file to the artifact.

--- libidn2/installer/install.lua
+++ libidn2/installer/install.lua
@@ -7,6 +7,7 @@ strDistVersion == '@JONCHKI_PLATFORM_DIST_VERSION@' and
 strCpuArch == '@JONCHKI_PLATFORM_CPU_ARCH@' then
   t:install_dev('include',    '${install_dev_include}/')
   t:install_dev('lib',        '${install_dev_lib}/')
+  t:install_dev('pkgconfig',  '${install_dev}/pkgconfig/')
   tResult = true
 end
 
